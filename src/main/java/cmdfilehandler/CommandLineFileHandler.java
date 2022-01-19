d4d8ef4bbb600240c14b2c9649077809fe345c95
LucasKienast <lucas.kienast@hotmail.de>
2022-01-19 19:43:49 +0100

    Created simple welcome console output

---
+++ src/main/java/cmdfilehandler/CommandLineFileHandler.java
@@ -0,0 +1,25 @@
+package cmdfilehandler;
+
+import java.util.Scanner;
+
+public class CommandLineFileHandler {
+
+	public static void main(String[] args) {
+		System.out.println("_______COMMAND_LINE_FILE_HANDLER_______");
+		System.out.println("_________(BY_MAX_LUCAS_KIENAST)________");
+		System.out.println();
+		System.out.println("Please choose an option");
+		System.out.println("1) List all files");
+		System.out.println("2) Search for files");
+		System.out.println("3) View a file");
+		System.out.println("4) Edit a file");
+		System.out.println("5) Delete a file");
+		System.out.println("6) Delete ALL files");
+		System.out.println("7) Close this application");
+
+		Scanner sc = new Scanner(System.in);
+		int userInput;
+		
+	}
+
+}
